113dbb90e659a925f65706a411c704d70080ccc5
gudzpoz@live.com <gudzpoz@live.com>
2025-01-14 17:51:08 +0800

    Format org-mode timestamps as fancydates

--- conf.el
+++ conf.el
@@ -1,3 +1,19 @@
 (setq org-export-use-babel nil
       org-export-with-toc t
       org-export-with-section-numbers t)
+
+(defun clear-single-linebreak-in-cjk-string (string)
+  "clear single line-break between cjk characters that is usually soft line-breaks"
+  (let* ((regexp "\\([\u4E00-\u9FA5]\\)[ \n]+\\([\u4E00-\u9FA5]\\)")
+         (start (string-match regexp string)))
+    (while start
+      (setq string (replace-match "\\1\\2" nil nil string)
+            start (string-match regexp string start))))
+  string)
+
+(defun ox-html-clear-single-linebreak-for-cjk (string backend info)
+  (when (org-export-derived-backend-p backend 'html)
+    (clear-single-linebreak-in-cjk-string string)))
+
+(add-to-list 'org-export-filter-final-output-functions
+             'ox-html-clear-single-linebreak-for-cjk)
