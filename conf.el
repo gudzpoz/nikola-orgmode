86c7f425e24cb3f9b23f7705fcc68369425ab27b
gudzpoz@live.com <gudzpoz@live.com>
2025-01-17 03:31:06 +0800

    Allow customizing footnote positions

--- conf.el
+++ conf.el
@@ -1,12 +1,37 @@
+;; Org configs
 (setq org-export-use-babel nil
       org-export-with-toc t
       org-export-with-section-numbers t)
 
-(package-vc-install "https://github.com/jeffkreeftmeijer/ox-html-stable-ids.el.git")
-(use-package ox-html-stable-ids
+(setq package-load-list '(all))
+(package-initialize)
+;; Semantic HTML heading ID
+(use-package ox-html-stable-ids :ensure
+  :vc (:url "https://codeberg.org/jkreeftmeijer/ox-html-stable-ids.el.git")
   :config (org-html-stable-ids-add)
   :custom (org-html-stable-ids t))
 
+;; Semantic ID for Chinese chars
+(defvar pinyin-inverse-map (make-char-table nil)
+  "A char-table mapping from chars to their Chinese pinyin.")
+(with-temp-buffer
+  (quail-use-package "chinese-py" "quail/PY")
+  (let ((decode-map (list 'decode-map)))
+    (quail-build-decode-map (list (quail-map)) "" decode-map 0 most-positive-fixnum)
+    (dolist (pair (cdr decode-map))
+      (pcase-let ((`(,pinyin . ,chars) pair))
+        (if (fixnump chars)
+            (aset pinyin-inverse-map chars pinyin)
+          (cl-loop for zh-char across chars do
+                   (aset pinyin-inverse-map (string-to-char zh-char) pinyin)))))))
+(defun char-to-pinyin (c)
+  (if-let ((pinyin (aref pinyin-inverse-map c)))
+      (concat " " pinyin " ")
+    (string c)))
+(define-advice org-html-stable-ids--to-kebab-case (:around (orig string))
+  (funcall orig (mapconcat #'char-to-pinyin string)))
+
+;; Line breaks
 (defun clear-single-linebreak-in-cjk-string (string)
   "clear single line-break between cjk characters that is usually soft line-breaks"
   (let* ((regexp "\\([\u4E00-\u9FA5]\\)[ \n]+\\([\u4E00-\u9FA5]\\)")
@@ -15,10 +40,58 @@
       (setq string (replace-match "\\1\\2" nil nil string)
             start (string-match regexp string start))))
   string)
-
 (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
   (when (org-export-derived-backend-p backend 'html)
     (clear-single-linebreak-in-cjk-string string)))
-
 (add-to-list 'org-export-filter-final-output-functions
              'ox-html-clear-single-linebreak-for-cjk)
+
+;; Footnotes
+(setq footnote-definitions (make-hash-table :test 'eq))
+(defun org-nikola--find-footnote-def (footnote info)
+  (let ((footnote-defs (with-memoization
+                           (gethash info footnote-definitions)
+                         (org-export-collect-footnote-definitions info)))
+        (inner (cddr footnote)))
+    (cl-block loop
+      (cl-dolist (def footnote-defs)
+        (when (eq (nth 2 def) inner)
+          (cl-return def))))))
+(defsubst org-footnote--label-id (label n)
+  ;; Do not assign number labels as they appear in Org mode
+  ;; - the footnotes are re-numbered by
+  ;; `org-export-get-footnote-number'.  If the label is not
+  ;; a number, keep it.
+  (if (and (stringp label)
+           (equal label (number-to-string (string-to-number label))))
+      n
+    label))
+(defun org-nikola-footnote-definition (footnote contents info)
+  (pcase-let ((`(,n ,label ,def) (org-nikola--find-footnote-def footnote info)))
+    (setq label (org-footnote--label-id label n))
+    (let ((anchor (org-html--anchor
+                   (format "fn.%s" label)
+                   n
+                   (format " class=\"footnum\" href=\"#fnr.%s\" role=\"doc-backlink\"" label)
+                   info))
+          (contents (org-trim (org-export-data def info))))
+      (format "<div class=\"footdef\">%s %s</div>\n"
+              (format (plist-get info :html-footnote-format) anchor)
+              (format "<div class=\"footpara\" role=\"doc-footnote\">%s</div>" contents)))))
+;; Add footnote definition handler
+(let* ((backend (org-export-get-backend 'html))
+       (transcoders (org-export-backend-transcoders backend)))
+  (setf (org-export-backend-transcoders backend)
+        (cons '(footnote-definition . org-nikola-footnote-definition)
+              transcoders)))
+;; Handle inline footnotes
+(define-advice org-html-footnote-reference (:around (orig footnote contents info))
+  (let ((def (org-nikola--find-footnote-def footnote info))
+        (ref (funcall orig footnote contents info)))
+    (if def
+        (let ((label (org-footnote--label-id (cadr def) (car def))))
+          (format "<span id=\"fn.%s\" class=\"inline-footdef\"> (%s %s) </span>"
+                  label ref contents))
+      ref)))
+;; Disable footnote sections
+(define-advice org-html-footnote-section (:override (&rest _)) nil)
